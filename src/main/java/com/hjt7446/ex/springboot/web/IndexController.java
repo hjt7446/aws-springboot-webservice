dd1defcaf2fdc8ce56d612f7c9630787f49997f1
hjt7446 <hjt1120@naver.com>
2021-03-31 15:23:36 +0900

    Naver 로그인, 기존테스트에 스프링시큐리티 적용

--- src/main/java/com/hjt7446/ex/springboot/web/IndexController.java
+++ src/main/java/com/hjt7446/ex/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hjt7446.ex.springboot.web;
 
+import com.hjt7446.ex.springboot.config.auth.LoginUser;
 import com.hjt7446.ex.springboot.config.auth.dto.SessionUser;
 import com.hjt7446.ex.springboot.service.posts.PostsService;
 import com.hjt7446.ex.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        /*SessionUser user = (SessionUser) httpSession.getAttribute("user");*/
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
